7f39019d35df2fa9305d7aee26e89080b82b7b46
dampedboy <dampededits@gmail.com>
2024-06-07 16:50:26 +0200

    Sistemazione merge e varie cose

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,24 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed=5f;
+    [SerializeField] private int timeToLive = 10;
+
 
     private void Update()
     {
-        transform.Translate(new Vector3(0f,0f,projectileSpeed*Time.deltaTime));
+
+        while (timeToLive > 0)
+        {
+            timeToLive -= Convert.ToInt32(Time.deltaTime);
+            Debug.Log(timeToLive + " Fuck you unity");
+            if (timeToLive == 0)
+            {
+                Debug.Log("Kill mee");
+            }
+            
+        }
+        transform.Translate(new Vector3(0f, 0f, projectileSpeed * Time.deltaTime));
+
     }
     
 }
