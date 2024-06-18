38c61ab1f3c5c0a86fcc83a2c065e683f94fb77c
dampedboy <dampededits@gmail.com>
2024-06-18 16:11:44 +0200

    Robe varie che non mi ricordo

--- Assets/Portale.cs
+++ Assets/Portale.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Portale : MonoBehaviour
 {
-    public Collider oggettoDaTeletrasportare;
+    //public Collider oggettoDaTeletrasportare;
     public GameObject altroPortale;
+     [SerializeField] string nomeOggetto="";
     // Start is called before the first frame update
     void Start()
     {
-        
+        //oggettoDaTeletrasportare = new Collider();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(oggettoDaTeletrasportare.ToString()+" è entrato");
+        Debug.Log(this.GetComponent<Collider>().isTrigger+"is trigger");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        oggettoDaTeletrasportare = other;
+        if (other.GetComponentInParent<Transform>())
+        {
+
+        }
+        Debug.Log(other.name + " è entrato");
     }
 }
