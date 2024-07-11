55bd4584f5997903bd37a475ab2c7615e98aa0a0
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-11 21:44:21 +0200

    giocatore bloccato durante dialoghi

--- Assets/Scripts/ScriptDialogueSystem/Player.cs
+++ Assets/Scripts/ScriptDialogueSystem/Player.cs
@@ -7,19 +7,30 @@ public class Player : MonoBehaviour
     [SerializeField] private DialogueUI dialogueUI;
     public DialogueUI DialogueUI => dialogueUI;
 
-    public IInteractable Interactable { get;  set; }
+    public IInteractable Interactable { get; set; }
+
+    private PlayerMovement playerMovementScript;
 
     void Start()
     {
+        playerMovementScript = GetComponent<PlayerMovement>();
     }
 
     private void Update()
     {
-        if (dialogueUI.IsOpen) return; 
+        if (dialogueUI.IsOpen)
+        {
+            playerMovementScript.enabled = false;
+        }
+        else
+        {
+            playerMovementScript.enabled = true;
+
+        }
 
         if (Input.GetKeyDown(KeyCode.O))
         {
-            if(Interactable != null)
+            if (Interactable != null)
             {
                 Interactable.Interact(this);
             }
