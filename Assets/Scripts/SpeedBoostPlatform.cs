15fcd7a52a3f28adebbe9d152fedcf5bca0c21db
NiccoBram <158487655+NiccoBram@users.noreply.github.com>
2024-07-13 22:37:44 +0200

    Sistemazione Slow e Fast Platform

--- Assets/Scripts/SpeedBoostPlatform.cs
+++ Assets/Scripts/SpeedBoostPlatform.cs
@@ -1,23 +1,19 @@
-using StarterAssets;
 using UnityEngine;
 
 public class SpeedBoostPlatform : MonoBehaviour
 {
     public float boostSpeed = 10.0f; // La velocità aumentata quando il player è sulla piattaforma
     private float normalSpeed;
-    private float normalSprintSpeed;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            ThirdPersonController playerController = other.GetComponent<ThirdPersonController>();
-            if (playerController != null)
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+            if (playerMovement != null)
             {
-                normalSpeed = playerController.MoveSpeed; // Salva la velocità normale
-                normalSprintSpeed = playerController.SprintSpeed; // Salva la velocità di sprint normale
-                playerController.MoveSpeed = boostSpeed; // Imposta la velocità aumentata
-                playerController.SprintSpeed = boostSpeed; // Imposta la velocità di sprint aumentata
+                normalSpeed = playerMovement.maximumSpeed; // Salva la velocità normale
+                playerMovement.maximumSpeed = boostSpeed; // Imposta la velocità aumentata
             }
         }
     }
@@ -26,11 +22,10 @@ public class SpeedBoostPlatform : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            ThirdPersonController playerController = other.GetComponent<ThirdPersonController>();
-            if (playerController != null)
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+            if (playerMovement != null)
             {
-                playerController.MoveSpeed = normalSpeed; // Ripristina la velocità normale
-                playerController.SprintSpeed = normalSprintSpeed; // Ripristina la velocità di sprint normale
+                playerMovement.maximumSpeed = normalSpeed; // Ripristina la velocità normale
             }
         }
     }
