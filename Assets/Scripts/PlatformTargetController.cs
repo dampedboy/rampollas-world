0a2d6c043876bf8f4afcfc4e1f0daa06191449d7
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-14 21:35:32 +0200

    sistemate piattaforme lvl 5 e 10

--- Assets/Scripts/PlatformTargetController.cs
+++ Assets/Scripts/PlatformTargetController.cs
@@ -15,33 +15,23 @@ public class PlatformTargetController : MonoBehaviour
     private bool isTriggered = false;
 
     // Velocità di caduta del blocco
-    public float dropSpeed = 5.0f;
+    public float dropSpeed = 8.0f;
 
-    // Tempo di ritardo prima della caduta in secondi (3 secondi)
-    private float delay = 2.0f; // 3 secondi
 
-    // Timer per il ritardo
-    private float delayTimer;
 
     void Start()
     {
         // Imposta la posizione iniziale del blocco
         block.transform.position = OriginalPosition.position;
         // Inizializza il timer di ritardo
-        delayTimer = delay;
     }
 
     void Update()
     {
         if (isTriggered)
         {
-            // Riduce il timer di ritardo fino a 0
-            if (delayTimer > 0)
-            {
-                delayTimer -= Time.deltaTime;
-            }
-            else
-            {
+
+
                 // Movimento del blocco verso la posizione della piattaforma
                 block.transform.position = Vector3.MoveTowards(block.transform.position, PlatformPosition.position, dropSpeed * Time.deltaTime);
 
@@ -51,7 +41,7 @@ public class PlatformTargetController : MonoBehaviour
                     // Blocca il movimento quando raggiunge la piattaforma
                     isTriggered = false;
                 }
-            }
+
         }
     }
 
