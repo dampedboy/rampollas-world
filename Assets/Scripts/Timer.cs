9acf81502cb3f1bc436080824c7ee4d2a1f28ca7
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-15 14:53:02 +0200

    sistemato timer e falling point lvl 5

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,71 +6,36 @@ public class Timer : MonoBehaviour
 {
     public float respawnTime = 30f; // Tempo di respawn in secondi
     public TextMeshProUGUI timerText; // Testo del timer
-    public TextMeshProUGUI gameOverText; // Testo per la scritta Game Over
 
     private float timer; // Timer di respawn
-    private bool isGameOver = false; // Flag per indicare se il gioco è finito
 
     void Start()
     {
         timer = respawnTime;
         UpdateTimerText();
-        gameOverText.gameObject.SetActive(false); // Nascondi il testo Game Over inizialmente
     }
 
     void Update()
     {
-        if (!isGameOver)
+        timer -= Time.deltaTime; // Sottrai il tempo trascorso dal timer
+
+        if (timer <= 0f)
         {
-            timer -= Time.deltaTime; // Sottrai il tempo trascorso dal timer
-
-            if (timer <= 0f)
-            {
-                GameOver(); // Chiamata alla funzione di Game Over quando il timer arriva a zero
-            }
-
-            UpdateTimerText(); // Aggiorna il testo del timer ogni frame
+            ReloadCurrentScene(); // Chiamata alla funzione per ricaricare la scena corrente
         }
+
+        UpdateTimerText(); // Aggiorna il testo del timer ogni frame
     }
 
-    void GameOver()
+    void ReloadCurrentScene()
     {
-        isGameOver = true;
-        timer = 0f; // Assicurati che il timer sia esattamente zero
-        gameOverText.gameObject.SetActive(true); // Mostra il testo Game Over
-        Invoke("LoadHubScene", 3f); // Carica la scena Hub Centrale dopo 3 secondi
-    }
-
-    void LoadHubScene()
-    {
-        if (SceneExists("Hub"))
-        {
-            SceneManager.LoadScene("Hub"); // Carica la scena Hub Centrale
-        }
-        else
-        {
-            Debug.LogError("La scena 'Hub' non esiste o non è stata aggiunta alle build settings!");
-        }
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName); // Ricarica la scena corrente
     }
 
     void UpdateTimerText()
     {
         int seconds = Mathf.CeilToInt(timer);
         timerText.text = seconds.ToString(); // Aggiorna il testo del timer con i secondi rimanenti
-    }
-
-    // Funzione per verificare se una scena esiste nelle build settings
-    private bool SceneExists(string sceneName)
-    {
-        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
-        {
-            string path = SceneUtility.GetScenePathByBuildIndex(i);
-            string scene = System.IO.Path.GetFileNameWithoutExtension(path);
-            if (scene == sceneName)
-            {
-                return true;
-            }
-        }
-        return false;
     }
 }
