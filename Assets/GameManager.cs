d5fe8d08afee7b80a448637ec017aec3c290862b
Baffaloo <gnicomars@gmail.com>
2024-07-17 23:22:10 +0200

    UPDATE 2

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,6 +6,19 @@ public class GameManager : MonoBehaviour
     public static bool enteringFromTrapdoor = false;
     private static GameManager instance;
     
+     private void Awake()
+    {
+        // Assicurati che ci sia solo una istanza di GameManager
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Non distruggere questo oggetto quando si cambia scena
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Metodo per impostare l'ingresso dalla botola
     public static void EnterFromTrapdoor()
     {
