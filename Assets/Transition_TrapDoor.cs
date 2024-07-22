c81c98339217567714fc094e655aabf0456707ad
ernygru <127400398+ernygru@users.noreply.github.com>
2024-07-22 05:13:17 +0200

    cambiamenti 3

--- Assets/Transition_TrapDoor.cs
+++ Assets/Transition_TrapDoor.cs
@@ -5,12 +5,25 @@ using UnityEngine.SceneManagement;
 public class Transition_TrapDoor : MonoBehaviour
 {
     public string sceneName;
+    
+    public Animator transition;
+    public float transitionTime = 1f;
 
     void OnTriggerEnter(Collider other){
         if(other.CompareTag("Player")){
             GameManager.JustNo();
             GameManager.EnterFromTrapdoor();
             SceneManager.LoadScene(sceneName);
+            //StartCoroutine(LoadLevel(sceneName));
         }
+    }
+    
+    IEnumerator LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+        
+        SceneManager.LoadScene(sceneName);
     }
 }
