7ba7b0287fdfb8c6a7b86571e429d2bafcc30662
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-29 22:20:58 +0200

    sistemato suono esplosione

--- Assets/Scripts/Dynamite.cs
+++ Assets/Scripts/Dynamite.cs
@@ -6,26 +6,30 @@ public class Dynamite : MonoBehaviour
     public float explosionRadius = 5f;
     public AudioClip esplosione; // AudioClip per l'esplosione
     private AudioSource audioSource;
+    private MeshRenderer meshRenderer;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = esplosione;
+        meshRenderer = GetComponent<MeshRenderer>(); // Ottieni il MeshRenderer del gameObject
     }
 
     void OnTriggerEnter(Collider other)
     {
-
-        
         if (other.CompareTag("Enemy"))
         {
             audioSource.Play();
             Destroy(other.gameObject); // Distruggi il nemico
-            float delay = Mathf.Max(audioSource.clip.length - 1.55f, 0f); // Calcola il tempo di ritardo, assicurandoti che non sia negativo
-            audioSource.Play();
 
+            // Disattiva il MeshRenderer della dinamite
+            if (meshRenderer != null)
+            {
+                meshRenderer.enabled = false;
+            }
+
+            float delay = Mathf.Max(audioSource.clip.length, 0f);
             Destroy(gameObject, delay); // Distruggi la dinamite dopo il tempo di ritardo calcolato
-
         }
     }
 }
