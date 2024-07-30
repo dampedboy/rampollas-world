4c8e3dea30e53c8c63274c25ee7be60472fdc3ea
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-30 19:12:03 +0200

    suono chiusura botola

--- Assets/Scripts/Trap_Door.cs
+++ Assets/Scripts/Trap_Door.cs
@@ -9,15 +9,31 @@ public class Trap_Door : MonoBehaviour
     public Transform door;
     public Transform player;
 
+    public AudioClip trapdoorCloseSound; // Aggiungi questa riga per l'audio clip
+    private AudioSource audioSource; // Aggiungi questa riga per l'AudioSource
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>(); // Inizializza l'AudioSource
+    }
+
     void Update()
     {
         float distance = Vector3.Distance(player.position,door.position);
         if (distance<=6){
             doorAnim.SetBool("Near",true);
+            PlayTrapdoorCloseSound(); // Riproduce il suono di apertura botola
         }
-        else{
+        else
+        {
             doorAnim.SetBool("Near",false);
         }
+    }
+
+    void PlayTrapdoorCloseSound()
+    {
+        audioSource.clip = trapdoorCloseSound;
+        audioSource.Play();
     }
 
 }
