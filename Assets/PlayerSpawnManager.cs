286ec332f428031003f0e50e7ece7bac87f2a4cc
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-07-30 19:17:31 +0200

    aggiunto suono spawn botola

--- Assets/PlayerSpawnManager.cs
+++ Assets/PlayerSpawnManager.cs
@@ -12,23 +12,33 @@ public class PlayerSpawnManager : MonoBehaviour
     public Transform ButtonPosition;
     public Transform PortalPosition;
 
+    // Riferimento al suono di spawn dalla botola
+    public AudioClip spawnBotolaClip;
+    private AudioSource audioSource;
 
     void Start()
     {
+        // Ottieni il componente AudioSource
+        audioSource = GetComponent<AudioSource>();
+
         // Ottieni il personaggio
         GameObject player = GameObject.FindWithTag("Player");
 
-        if(GameManager.enteringFromColor) player.transform.position = ColorPosition.position;
-        if(GameManager.enteringFromAction) player.transform.position = ActionPosition.position;
-        if(GameManager.enteringFromBreak) player.transform.position = BreakPosition.position;
-        if(GameManager.enteringFromButton) player.transform.position = ButtonPosition.position;
-        if(GameManager.enteringFromPortal) player.transform.position = PortalPosition.position;
-
+        if (GameManager.enteringFromColor) player.transform.position = ColorPosition.position;
+        if (GameManager.enteringFromAction) player.transform.position = ActionPosition.position;
+        if (GameManager.enteringFromBreak) player.transform.position = BreakPosition.position;
+        if (GameManager.enteringFromButton) player.transform.position = ButtonPosition.position;
+        if (GameManager.enteringFromPortal) player.transform.position = PortalPosition.position;
 
         // Controlla lo stato di ingresso e imposta la posizione iniziale
         if (GameManager.enteringFromTrapdoor)
         {
             player.transform.position = trapdoorSpawnPosition.position;
+            // Riproduci il suono di spawn dalla botola
+            if (spawnBotolaClip != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(spawnBotolaClip);
+            }
         }
         else
         {
@@ -36,7 +46,6 @@ public class PlayerSpawnManager : MonoBehaviour
             {
                 player.transform.position = doorSpawnPosition.position;
             }
-            
         }
     }
 }
