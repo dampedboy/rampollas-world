8c982b96c2a07fc4e8659c04d36b5973703bd01d
Nali12345 <147627584+Nali12345@users.noreply.github.com>
2024-08-12 18:04:26 +0200

    musica che si ferma

--- Assets/Scripts/MusicManagment/MusicPlayer.cs
+++ Assets/Scripts/MusicManagment/MusicPlayer.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
     public static MusicPlayer Instance;
-    
+
     public AudioSource audioSource;      // La sorgente audio da cui verranno riprodotti i clip
     public AudioClip[] audioClips;       // L'array di clip audio
     public float fadeDuration = 1.0f;    // Durata della dissolvenza in secondi
 
     private int currentClipIndex = 0;
-    
+
     private void Awake()
     {
         if (Instance == null)
@@ -31,6 +32,37 @@ public class MusicPlayer : MonoBehaviour
         {
             StartCoroutine(PlayNextClip());
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded; // Aggiungi l'evento per monitorare il cambio di scena
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Verifica se la scena corrente è tra quelle in cui la musica deve essere messa in pausa
+        if (scene.buildIndex >= 25 && scene.buildIndex <= 30)
+        {
+            PauseMusic();
+        }
+        else
+        {
+            ResumeMusic();
+        }
+    }
+
+    private void PauseMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+    }
+
+    private void ResumeMusic()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.UnPause();
+        }
     }
 
     private IEnumerator PlayNextClip()
@@ -39,9 +71,9 @@ public class MusicPlayer : MonoBehaviour
         {
             audioSource.clip = audioClips[currentClipIndex];
             audioSource.Play();
-            
+
             Debug.Log("sto suonando la traccia " + currentClipIndex);
-            
+
             yield return StartCoroutine(FadeIn(audioSource, fadeDuration));
 
             yield return new WaitForSeconds(audioSource.clip.length - fadeDuration);
@@ -82,5 +114,9 @@ public class MusicPlayer : MonoBehaviour
 
         audioSource.volume = 0f;
     }
-}
 
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded; // Rimuovi l'evento quando l'oggetto viene distrutto
+    }
+}
