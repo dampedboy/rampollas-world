b45c36aff1b04fdc9c88cdd061b5098ac43e9222
Baffaloo <gnicomars@gmail.com>
2024-09-07 19:19:03 +0200

    AGGIORNATO ROBA

--- Assets/BookTrainingScript.cs
+++ Assets/BookTrainingScript.cs
@@ -15,13 +15,13 @@ public class BookTrainingScript : MonoBehaviour
         // Controlla la distanza tra il player e l'oggetto
         if (Vector3.Distance(player.transform.position, transform.position) < interactionDistance)
         {
-            // Se il giocatore preme il tasto T o il pulsante in alto del controller (Jump) e non sta già visualizzando immagini
-            if ((Input.GetKeyDown(KeyCode.O) || Input.GetButtonDown("Fire3")) && !isViewingImages)
+            // Se il giocatore preme il tasto I o il pulsante in alto del controller (Jump) e non sta già visualizzando immagini
+            if ((Input.GetKeyDown(KeyCode.I) || Input.GetButtonDown("Fire1")) && !isViewingImages)
             {
                 StartViewingImages();
             }
-            // Se il giocatore preme il tasto T o il pulsante in alto del controller (Jump) e sta visualizzando immagini
-            else if ((Input.GetKeyDown(KeyCode.O) || Input.GetButtonDown("Fire3")) && isViewingImages)
+            // Se il giocatore preme il tasto I o il pulsante in alto del controller (Jump) e sta visualizzando immagini
+            else if ((Input.GetKeyDown(KeyCode.I) || Input.GetButtonDown("Fire1")) && isViewingImages)
             {
                 ShowNextImage();
             }
